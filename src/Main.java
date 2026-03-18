bd0d8152b3212b4e8526aea736e1d0202c769d0f
Tomás Poim <tomas.poim14@gmail.com>
2026-03-18 20:36:40 +0000

    I think I might be close

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
--- src/problem/Adventure.java
+++ src/problem/Adventure.java
@@ -9,7 +9,7 @@ public class Adventure {
         final int overConsecutiveJumps = land.maxConsecutiveJumps + 1;
         final int overJumpLimit = land.jumpLimit + 1;
         // long is initialized to 0
-        long[][][][] dp = new long[land.rows][land.columns][overConsecutiveJumps + 1][overJumpLimit + 1];
+        long[][][][] dp = new long[land.rows][land.columns][overConsecutiveJumps][overJumpLimit];
 
         // if I'm at the castle tile within the jump limits, I have one path
         for(int i = 0; i < overConsecutiveJumps; i++) {
@@ -21,22 +21,23 @@ public class Adventure {
         for(int row = land.rows - 1; row >= 0 ; row--) {
             for(int col = land.columns - 1; col >= 0; col--) {
                 // skip castle case
-                if(atCastle(land, row, col)) {
+                if ((row == land.rows - 1) && (col == land.columns - 1)) {
                     continue;
                 }
 
                 // landing on quicksand, no matter what, will give you 0
-                if(inQuickSand(land, row, col)) {
+                Tile tile = land.tiles[row][col];
+                if (tile.type == Tile.Type.QUICK_SAND) {
                     continue;
                 }
 
-                for(int consecutiveJumps = 0; consecutiveJumps <= land.maxConsecutiveJumps; consecutiveJumps++) {
+                for(int consecutiveJumps = 0; consecutiveJumps < overConsecutiveJumps; consecutiveJumps++) {
                     // you cannot have more jumps than current row
                     if(consecutiveJumps > row) {
                         break;
                     }
 
-                    for(int totalJumps = consecutiveJumps; totalJumps <= land.jumpLimit; totalJumps++) {
+                    for(int totalJumps = consecutiveJumps; totalJumps < overJumpLimit; totalJumps++) {
                         // same as above
                         if(totalJumps > row) {
                             break;
@@ -44,22 +45,22 @@ public class Adventure {
 
                         long totalPaths = 0;
                         // conversion is 1:1 with recursive
-                        if(!isOutOfBounds(land, row, col + 1)) {
+                        if(!((row >= land.rows) || (col + 1 >= land.columns) || (row < 0) || (col + 1 < 0))) {
                             totalPaths = (totalPaths + dp[row][col + 1][0][totalJumps]) % MODULO; // R
                         }
-                        if(!isOutOfBounds(land, row + 1, col)) {
+                        if(!((row + 1 >= land.rows) || (col >= land.columns) || (row + 1 < 0) || (col < 0))) {
                             totalPaths = (totalPaths + dp[row + 1][col][0][totalJumps]) % MODULO; // D
                         }
-                        if(isJumpClear(land, row, col) && consecutiveJumps < land.maxConsecutiveJumps && totalJumps < land.jumpLimit) {
-                            if(!isOutOfBounds(land, row + 2, col)) {
+                        if((tile.type != Tile.Type.JUMP_BARRIER) && consecutiveJumps < land.maxConsecutiveJumps && totalJumps < land.jumpLimit) {
+                            if(!((row + 2 >= land.rows) || (col >= land.columns) || (row + 2 < 0) || (col < 0))) {
                                 totalPaths = (totalPaths + dp[row + 2][col][consecutiveJumps + 1][totalJumps + 1]) % MODULO; // DD
                             }
-                            if(isDiagonalClear(land, row, col)) {
-                                if(!isOutOfBounds(land, row + 1, col - 1)) {
+                            if(tile.type != Tile.Type.DIAGONAL_BARRIER) {
+                                if(!((row + 1 >= land.rows) || (col - 1 >= land.columns) || (row + 1 < 0) || (col - 1 < 0))) {
                                     totalPaths = (totalPaths + dp[row + 1][col - 1][consecutiveJumps + 1][totalJumps + 1]) % MODULO; // LD
                                 }
-                                if(!isOutOfBounds(land, row + 1, col + 1)) {
-                                    totalPaths = (totalPaths + dp[row + 1][col + 1][consecutiveJumps + 1][totalJumps + 1]) % MODULO; // LD
+                                if(!((row + 1 >= land.rows) || (col + 1 >= land.columns) || (row + 1 < 0) || (col + 1 < 0))) {
+                                    totalPaths = (totalPaths + dp[row + 1][col + 1][consecutiveJumps + 1][totalJumps + 1]) % MODULO; // RD
                                 }
                             }
                         }
@@ -70,52 +71,5 @@ public class Adventure {
         }
 
         return dp[0][0][0][0];
-    }
-
-    static boolean atCastle(final Land land, final int row, final int col) {
-        if ((row == land.rows - 1) && (col == land.columns - 1)) {
-            return true;
-        }
-
-        return false;
-    }
-
-    static boolean inQuickSand(final Land land, final int row, final int col) {
-        Tile tile = land.tiles[row][col];
-        if (tile.type == Tile.Type.QUICK_SAND) {
-            return true;
-        }
-
-        return false;
-    }
-
-    static boolean isDiagonalClear(final Land land, final int row, final int col) {
-        Tile tile = land.tiles[row][col];
-        if (tile.type == Tile.Type.DIAGONAL_BARRIER) {
-            return false;
-        }
-
-        return true;
-    }
-
-    static boolean isJumpClear(final Land land, final int row, final int col) {
-        Tile tile = land.tiles[row][col];
-        if (tile.type == Tile.Type.JUMP_BARRIER) {
-            return false;
-        }
-
-        return true;
-    }
-
-    static boolean isOutOfBounds(final Land land, final int row, final int col) {
-        if ((row >= land.rows) || (col >= land.columns)) {
-            return true;
-        }
-
-        if ((row < 0) || (col < 0)) {
-            return true;
-        }
-
-        return false;
     }
 }
